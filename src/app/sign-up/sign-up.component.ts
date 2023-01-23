464aca95e9004b53dca38a4263bb3938b29af294
Apollo02112022 <nicolas.fuente@sncf.fr>
2023-01-23 15:06:43 +0100

    second commit

---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { User } from '../user';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent {
+  
+  model: User = new User("", "", "", "");
+
+  constructor() {} 
+
+  onSubmit(): void {
+    console.log(this.model);
+  } 
+  
+}
